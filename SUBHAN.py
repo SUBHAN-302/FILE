0dbccc94f89c11df830cf315fe3ec57cbf92f9c7
SUBHAN-302 <110691961+SUBHAN-302@users.noreply.github.com>
2022-10-10 23:07:33 +0500

    Add files via upload

---
+++ SUBHAN.py
@@ -0,0 +1,12 @@
+import platform
+bit=platform.architecture()[0]
+if bit =='64bit':
+bit = platform.architecture()[0]
+    from SUBHAN import main
+    subhan1()
+import requests
+except:
+    os.system('pip install requests')
+else:
+    print('Sorry Your Device Not Support Mr.SUBHAN Tools')
+    exit()
