7725ccf98fff71274dbb0c89c7d6284e54423220
SUBHAN-302 <110691961+SUBHAN-302@users.noreply.github.com>
2022-10-11 14:49:01 +0500

    Update SUBHAN.py

--- SUBHAN.py
+++ SUBHAN.py
@@ -3,7 +3,7 @@ bit=platform.architecture()[0]
 if bit =='64bit':
 
     from SUBHAN import main
-    subhan1()
+    menu()
 else:
     print('Sorry Your Device Not Support Mr.SUBHAN Tools')
     exit()
