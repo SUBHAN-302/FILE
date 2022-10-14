467d2cc088375063517fb207b3311ac95bf38ffb
SUBHAN-302 <110691961+SUBHAN-302@users.noreply.github.com>
2022-10-14 21:47:41 +0500

    Delete SUBHAN.py

--- SUBHAN.py
+++
@@ -1,9 +0,0 @@
-import platform
-bit=platform.architecture()[0]
-if bit =='64bit':
-
-    from SUBHAN import main
-    menu()
-else:
-    print('Sorry Your Device Not Support Mr.SUBHAN Tools')
-    exit()
