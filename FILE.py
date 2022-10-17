89f2f5a27c5a5e16b401231e871a84eff1212c4e
SUBHAN-302 <110691961+SUBHAN-302@users.noreply.github.com>
2022-10-17 19:58:01 +0500

    Delete FILE.py

--- FILE.py
+++
@@ -1,8 +0,0 @@
-import platform
-bit=platform.architecture()[0]
-if bit =='64bit':
-    from FILE import main
-    subhan()
-else:
-    print('Sorry Your Device Not Support Mr.SUBHAN Tools')
-    exit()
